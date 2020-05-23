ba3aa09a8d960c67f6cc46b8e8f644754c32ff2c
Ramaraj T <jsramraj@gmail.com>
2020-05-23 21:10:29 +0530

    add socket library and send the mouse events to the all connected clients

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-const express = require('express');
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 const { port } = require('./config');
 
-const app = express();
-app.use(express.static('public'))
-
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server running at port:${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('draw', function (data) {
+        console.log(data.type + ' at ' + data.x + ', ' + data.y);
+        socket.broadcast.emit('draw', data);
+    });
+    // socket.emit('news', { hello: 'world' });
+    // socket.on('my other event', (data) => {
+    //   console.log(data);
+    // });
+
 });
